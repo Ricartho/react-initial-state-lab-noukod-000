505498830f8ea849f8a44d75755c587ded16bc83
Ricartho Franck <ricartho.franck@esih.edu>
2019-10-01 16:57:35 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 export default class ImageSlider extends Component {
   constructor(){
     super()
-    this.state
+    this.state ={
+      
+    }
   }
   render(){
     return()
